cc331761d3b556be9af76b921d07da9cff05268c
Prabakaran Chenniappan(UST <291502@ust.com>
2025-04-18 16:04:24 +0530

    singlyLinkedList

--- src/list/Main.java
+++ src/list/Main.java
@@ -5,8 +5,21 @@ public class Main {
         SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
         singlyLinkedList.createList(5);
 
-        singlyLinkedList.insert(6, 0);
-        System.out.println(singlyLinkedList.head.value);
-        System.out.println(singlyLinkedList.head.next.value);
+        singlyLinkedList.insertElement(6, 0);
+        singlyLinkedList.insertElement(3, 5);
+        singlyLinkedList.insertElement(5, 1);
+        singlyLinkedList.insertElement(2, 3);
+        singlyLinkedList.insertElement(8, 4);
+
+        singlyLinkedList.searchElement(8);
+        singlyLinkedList.removeElementByValue(6);
+        singlyLinkedList.removeElementByPosition(0);
+
+        Node curr = singlyLinkedList.head;
+        while (curr != null) {
+            System.out.println(curr.value);
+            curr = curr.next;
+        }
+
     }
 }
--- src/list/SinglyLinkedList.java
+++ src/list/SinglyLinkedList.java
@@ -9,10 +9,11 @@ public class SinglyLinkedList {
         Node node = new Node();
         node.value = val;
         head = node;
+        tail = node;
         size = 1;
     }
 
-    public void insert(int nodeValue, int position) {
+    public void insertElement(int nodeValue, int position) {
         Node node = new Node();
         node.value = nodeValue;
         if (head == null) {
@@ -26,7 +27,7 @@ public class SinglyLinkedList {
         } else {
             Node tempNode = head;
             int index = 0;
-            while (index < position-1) {
+            while (index < position - 1) {
                 tempNode = tempNode.next;
                 index++;
             }
@@ -35,5 +36,61 @@ public class SinglyLinkedList {
             node.next = nextNode;
         }
         size++;
+    }
+
+    public void removeElementByValue(int nodeValue) {
+
+        if (head.value == nodeValue) {
+            head = head.next;
+            size--;
+            return;
+        }
+        Node prev = head;
+        Node curr = head.next;
+        while (curr.value != nodeValue) {
+            prev = curr;
+            curr = curr.next;
+        }
+
+        if (curr == tail) {
+            tail = prev;
+        }
+
+        prev.next = curr.next;
+        size--;
+    }
+
+    public void removeElementByPosition(int position) {
+
+        if (position == 0) {
+            head = head.next;
+        } else {
+            int index = 0;
+            Node prev = head;
+            Node curr = head.next;
+
+            while (index < position - 1) {
+                prev = curr;
+                curr = curr.next;
+                index++;
+            }
+            prev.next = curr.next;
+            size--;
+
+            if (position == size) {
+                tail = prev;
+            }
+        }
+    }
+
+    public void searchElement(int nodeValue) {
+
+        Node curr = head;
+        int index = 0;
+        while (curr.value != nodeValue) {
+            curr = curr.next;
+            index++;
+        }
+        System.out.println("Element found at index: " + index);
     }
 }
