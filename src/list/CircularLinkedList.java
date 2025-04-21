84a474160d74b84d3370fe6fe0f3249f2fea6311
Prabakaran Chenniappan(UST <291502@ust.com>
2025-04-21 14:54:04 +0530

    circularSinglyLinkedList

---
+++ src/list/CircularLinkedList.java
@@ -0,0 +1,96 @@
+package list;
+
+public class CircularLinkedList {
+
+    public Node head;
+    public Node tail;
+    public int size ;
+
+    public void createList(int nodeValue) {
+        Node node = new Node();
+        node.value = nodeValue;
+        node.next = node;
+        head = node;
+        tail = node;
+        size = 1;
+    }
+
+    public void insertElement(int nodeValue, int position) {
+        Node node = new Node();
+        node.value = nodeValue;
+        if (head == null) {
+            createList(nodeValue);
+            return;
+        } else if (position == 0) {
+            node.next = head;
+            head = node;
+            tail.next = head;
+        } else if (position == size) {
+            tail.next = node;
+            tail = node;
+            tail.next = head;
+        } else {
+            Node tempNode = head;
+            int index = 0;
+            while (index < position - 1) {
+                tempNode = tempNode.next;
+                index++;
+            }
+            Node nextNode = tempNode.next;
+            tempNode.next = node;
+            node.next = nextNode;
+        }
+        size++;
+    }
+
+    public void removeElementByPosition(int position) {
+
+        if (position < 0 || position >= size) {
+            System.out.println("Invalid position");
+            return;
+        }
+
+        if (position == 0) {
+            head = head.next;
+            tail.next = head;
+        } else {
+            int index = 0;
+            Node prev = head;
+            Node curr = head.next;
+            while (index < position - 1) {
+                prev = curr;
+                curr = curr.next;
+                index++;
+            }
+            if (curr == tail) {
+                tail = prev;
+                tail.next = head;
+                return;
+            }
+            prev.next = curr.next;
+        }
+        size--;
+    }
+
+    public void removeElementByValue(int nodeValue) {
+
+        if (head.value == nodeValue) {
+            head = head.next;
+            tail.next = head;
+        } else {
+            Node prev = head;
+            Node curr = head.next;
+            while (curr.value != nodeValue) {
+                prev = curr;
+                curr = curr.next;
+            }
+            if (curr == tail) {
+                tail = prev;
+                tail.next = head;
+                return;
+            }
+            prev.next = curr.next;
+        }
+        size--;
+    }
+}
--- src/list/Main.java
+++ src/list/Main.java
@@ -3,23 +3,42 @@ package list;
 public class Main {
     public static void main(String[] args) {
         SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
-        singlyLinkedList.createList(5);
-
+        
         singlyLinkedList.insertElement(6, 0);
-        singlyLinkedList.insertElement(3, 5);
-        singlyLinkedList.insertElement(5, 1);
+        singlyLinkedList.insertElement(3, 1);
+        singlyLinkedList.insertElement(5, 2);
         singlyLinkedList.insertElement(2, 3);
         singlyLinkedList.insertElement(8, 4);
 
-        singlyLinkedList.searchElement(8);
+        //singlyLinkedList.searchElement(8);
         singlyLinkedList.removeElementByValue(6);
         singlyLinkedList.removeElementByPosition(0);
 
+        System.out.println("Singly Linked List: ");
         Node curr = singlyLinkedList.head;
         while (curr != null) {
             System.out.println(curr.value);
             curr = curr.next;
         }
 
+
+        CircularLinkedList circularLinkedList = new CircularLinkedList();
+
+        circularLinkedList.insertElement(6, 0);
+        circularLinkedList.insertElement(3, 1);
+        circularLinkedList.insertElement(5, 2);
+        circularLinkedList.insertElement(2, 3);
+        circularLinkedList.insertElement(8, 4);
+        
+        circularLinkedList.removeElementByValue(6);
+        circularLinkedList.removeElementByPosition(2);
+
+        System.out.println("Circular Singly Linked List: ");
+        curr = circularLinkedList.head;
+        do {
+            System.out.println(curr.value);
+            curr = curr.next;
+        }
+        while (curr != circularLinkedList.head);
     }
 }
