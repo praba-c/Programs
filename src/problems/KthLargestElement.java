a628eb1ea9c6f1725d7739b06053c0babe59abcc
Prabakaran Chenniappan(UST <291502@ust.com>
2025-05-21 11:38:55 +0530

    Kth largest element

--- src/problems/KthLargestElement.java
+++ src/problems/KthLargestElement.java
@@ -1,4 +1,32 @@
 package problems;
 
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
 public class KthLargestElement {
+
+    public static void main(String[] args) {
+        int k = 3;
+        int[] arr = {2,5,1,4,8,8};
+
+        // BOTH THESE METHODS INCLUDES DUPLICATES.!!!
+        sort(arr, k);
+        queue(arr, k);
+    }
+
+    public static void sort(int[] arr, int k) {
+        Arrays.sort(arr);
+        System.out.println(arr[arr.length-k]);
+    }
+
+    public static void queue(int[] arr, int k) {
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        for (int num : arr) {
+            q.offer(num);
+            if (q.size() > k) {
+                q.poll();
+            }
+        }
+        System.out.println(q.peek());
+    }
 }
